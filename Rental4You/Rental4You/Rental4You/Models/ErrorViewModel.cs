1d7b0a533388156f5d82f7d4d0df456b8ba7929c
João Morais <72463113+joaormorais@users.noreply.github.com>
2022-12-10 02:19:41 +0000

    Delete Rental4You directory

--- Rental4You/Rental4You/Rental4You/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Rental4You.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
